90274a8dd0fea6b75ca5ac28cbd7b9131726391b
Konstantin Savosteev <Konstantin.Savosteev@virtoway.com>
2021-11-12 14:32:36 +0200

    PT-5025: Add Common provider abstraction (#3)
    feat: Add Common provider abstraction

---
+++ src/VirtoCommerce.Assets.Abstractions/ICommonBlobProvider.cs
@@ -0,0 +1,22 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+
+namespace VirtoCommerce.Assets.Abstractions
+{
+    public interface ICommonBlobProvider
+    {
+        string GetAbsoluteUrl(string blobKey);
+
+        Stream OpenRead(string blobUrl);
+
+        Task<Stream> OpenReadAsync(string blobUrl);
+
+        Stream OpenWrite(string blobUrl);
+
+        Task<Stream> OpenWriteAsync(string blobUrl);
+
+        bool Exists(string blobUrl);
+
+        Task<bool> ExistsAsync(string blobUrl);
+    }
+}
