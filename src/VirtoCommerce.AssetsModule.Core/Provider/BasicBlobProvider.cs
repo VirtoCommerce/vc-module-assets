ae105754fff292023d57f1c447e387c4ac122d1c
BelyakovSergey <Belyakov.Sergey@virtoway.com>
2021-12-05 23:28:51 +0300

    PT-5546: Whitelist for File Extensions

--- src/VirtoCommerce.AssetsModule.Core/Provider/BasicBlobProvider.cs
+++ src/VirtoCommerce.AssetsModule.Core/Provider/BasicBlobProvider.cs
@@ -16,11 +16,29 @@ namespace VirtoCommerce.AssetsModule.Core.Assets
             _settingsManager = settingsManager;
         }
 
+        // Blacklisted or whitelisted extension
         public virtual bool IsExtensionBlacklisted(string path)
         {
             var blackList = _platformOptions.Value.FileExtensionsBlackList.Union(
                 _settingsManager?.GetObjectSettingAsync(PlatformConstants.Settings.Security.FileExtensionsBlackList.Name).Result.AllowedValues.Cast<string>() ?? new string[0]);
-            return (blackList.Any(x => path.Trim().EndsWith(x.Trim(), StringComparison.OrdinalIgnoreCase)));
+            var blacklisted = (blackList.Any(x => path.Trim().EndsWith(x.Trim(), StringComparison.OrdinalIgnoreCase)));
+            var blackListCount = blackList.Count();
+
+            var whiteList = _platformOptions.Value.FileExtensionsWhiteList.Union(
+                _settingsManager?.GetObjectSettingAsync(PlatformConstants.Settings.Security.FileExtensionsWhiteList.Name).Result.AllowedValues.Cast<string>() ?? new string[0]);
+            var whitelisted = (whiteList.Any(x => path.Trim().EndsWith(x.Trim(), StringComparison.OrdinalIgnoreCase)));
+            var whiteListCount = whiteList.Count();
+
+            var result = false;
+            if(blacklisted) result = true;
+            else if(blackListCount == 0)
+            {
+                if (whitelisted) result = false;
+                else if (whiteListCount == 0) result = false;
+                else if (whiteListCount > 0 && !whitelisted) result = true;
+            }
+
+            return result;
         }
     }
 }
---
+++ tests/VirtoCommerce.AssetsModule.Tests/Provider/BasicBlobProviderTests.cs
@@ -0,0 +1,130 @@
+﻿using FluentAssertions;
+using Microsoft.Extensions.Options;
+using VirtoCommerce.Platform.Core;
+using Xunit;
+using Moq;
+using VirtoCommerce.Platform.Core.Settings;
+
+namespace VirtoCommerce.AssetsModule.Tests.Provider
+{
+    public class BasicBlobProviderTests
+    {
+        private readonly Mock<IOptions<PlatformOptions>> _platformOptionsMock;
+        private readonly Mock<ISettingsManager> _settingsManagerMock;
+        private readonly PlatformOptions _platformOptions;
+
+        public BasicBlobProviderTests()
+        {
+            _platformOptionsMock = new Mock<IOptions<PlatformOptions>>();
+            _settingsManagerMock = new Mock<ISettingsManager>();
+            _platformOptions = new PlatformOptions { FileExtensionsBlackList = new string[0] };
+            _platformOptions = new PlatformOptions { FileExtensionsWhiteList = new string[0] };
+            _platformOptionsMock.SetupGet(x => x.Value).Returns(_platformOptions);
+        }
+
+        [Fact]
+        public void IsExtensionBlacklisted_Blacklisted_ReturnTrue()
+        {
+            // Arrange
+            _settingsManagerMock.Setup(x => x.GetObjectSettingAsync(PlatformConstants.Settings.Security.FileExtensionsBlackList.Name, null, null)).ReturnsAsync(new ObjectSettingEntry()
+            {
+                AllowedValues = new string[] { ".pdf" }
+            });
+            _settingsManagerMock.Setup(x => x.GetObjectSettingAsync(PlatformConstants.Settings.Security.FileExtensionsWhiteList.Name, null, null)).ReturnsAsync(new ObjectSettingEntry()
+            {
+                AllowedValues = new string[0]
+            });
+            var service = new BasicBlobProviderMock(_platformOptionsMock.Object, _settingsManagerMock.Object);
+
+            // Act
+            var result = service.IsExtensionBlacklisted(".pdf");
+
+            // Assert
+            result.Should().BeTrue();
+        }
+
+        [Fact]
+        public void IsExtensionBlacklisted_NotWhitelisted_ReturnTrue()
+        {
+            // Arrange
+            _settingsManagerMock.Setup(x => x.GetObjectSettingAsync(PlatformConstants.Settings.Security.FileExtensionsBlackList.Name, null, null)).ReturnsAsync(new ObjectSettingEntry()
+            {
+                AllowedValues = new string[0]
+            });
+            _settingsManagerMock.Setup(x => x.GetObjectSettingAsync(PlatformConstants.Settings.Security.FileExtensionsWhiteList.Name, null, null)).ReturnsAsync(new ObjectSettingEntry()
+            {
+                AllowedValues = new string[] { ".txt" }
+            });
+            var service = new BasicBlobProviderMock(_platformOptionsMock.Object, _settingsManagerMock.Object);
+
+            // Act
+            var result = service.IsExtensionBlacklisted(".pdf");
+
+            // Assert
+            result.Should().BeTrue();
+        }
+
+        [Fact]
+        public void IsExtensionBlacklisted_Whitelisted_ReturnFalse()
+        {
+            // Arrange
+            _settingsManagerMock.Setup(x => x.GetObjectSettingAsync(PlatformConstants.Settings.Security.FileExtensionsBlackList.Name, null, null)).ReturnsAsync(new ObjectSettingEntry()
+            {
+                AllowedValues = new string[0]
+            });
+            _settingsManagerMock.Setup(x => x.GetObjectSettingAsync(PlatformConstants.Settings.Security.FileExtensionsWhiteList.Name, null, null)).ReturnsAsync(new ObjectSettingEntry()
+            {
+                AllowedValues = new string[] { ".txt" }
+            });
+            var service = new BasicBlobProviderMock(_platformOptionsMock.Object, _settingsManagerMock.Object);
+
+            // Act
+            var result = service.IsExtensionBlacklisted(".txt");
+
+            // Assert
+            result.Should().BeFalse();
+        }
+
+        [Fact]
+        public void IsExtensionBlacklisted_ListsIsEmpty_ReturnFalse()
+        {
+            // Arrange
+            _settingsManagerMock.Setup(x => x.GetObjectSettingAsync(PlatformConstants.Settings.Security.FileExtensionsBlackList.Name, null, null)).ReturnsAsync(new ObjectSettingEntry()
+            {
+                AllowedValues = new string[0]
+            });
+            _settingsManagerMock.Setup(x => x.GetObjectSettingAsync(PlatformConstants.Settings.Security.FileExtensionsWhiteList.Name, null, null)).ReturnsAsync(new ObjectSettingEntry()
+            {
+                AllowedValues = new string[0]
+            });
+            var service = new BasicBlobProviderMock(_platformOptionsMock.Object, _settingsManagerMock.Object);
+
+            // Act
+            var result = service.IsExtensionBlacklisted(".png");
+
+            // Assert
+            result.Should().BeFalse();
+        }
+
+        [Fact]
+        public void IsExtensionBlacklisted_BlacklistedAndWhitelisted_ReturnTrue()
+        {
+            // Arrange
+            _settingsManagerMock.Setup(x => x.GetObjectSettingAsync(PlatformConstants.Settings.Security.FileExtensionsBlackList.Name, null, null)).ReturnsAsync(new ObjectSettingEntry()
+            {
+                AllowedValues = new string[] { ".mp3" }
+            });
+            _settingsManagerMock.Setup(x => x.GetObjectSettingAsync(PlatformConstants.Settings.Security.FileExtensionsWhiteList.Name, null, null)).ReturnsAsync(new ObjectSettingEntry()
+            {
+                AllowedValues = new string[] { ".mp3" }
+            });
+            var service = new BasicBlobProviderMock(_platformOptionsMock.Object, _settingsManagerMock.Object);
+
+            // Act
+            var result = service.IsExtensionBlacklisted(".mp3");
+
+            // Assert
+            result.Should().BeTrue();
+        }
+    }
+}
