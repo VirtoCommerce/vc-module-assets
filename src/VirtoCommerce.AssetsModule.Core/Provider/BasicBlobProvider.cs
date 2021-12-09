eb7312b8944e6e7bf232679cd8f6614270f3b153
BelyakovSergey <88537215+BelyakovSergey@users.noreply.github.com>
2021-12-09 19:17:35 +0300

    PT-5546: Whitelist for File Extensions (#5)
    * PT-5546: Whitelist for File Extensions
    
    * PT-5546: Simplify conditions
    
    * PT-5546: Move to the private method to simplify tests
    
    * PT-5546: Do not specify the fully qualified type name
    
    * PT-5546: Change logic

--- src/VirtoCommerce.AssetsModule.Core/Provider/BasicBlobProvider.cs
+++ src/VirtoCommerce.AssetsModule.Core/Provider/BasicBlobProvider.cs
@@ -16,11 +16,29 @@ namespace VirtoCommerce.AssetsModule.Core.Assets
             _settingsManager = settingsManager;
         }
 
+        // Blacklisted or whitelisted extension
         public virtual bool IsExtensionBlacklisted(string path)
         {
             var blackList = _platformOptions.Value.FileExtensionsBlackList.Union(
                 _settingsManager?.GetObjectSettingAsync(PlatformConstants.Settings.Security.FileExtensionsBlackList.Name).Result.AllowedValues.Cast<string>() ?? new string[0]);
-            return (blackList.Any(x => path.Trim().EndsWith(x.Trim(), StringComparison.OrdinalIgnoreCase)));
+            var blacklisted = (blackList.Any(x => path.Trim().EndsWith(x.Trim(), StringComparison.OrdinalIgnoreCase)));
+            var blackListCount = blackList.Count();
+
+            var whiteList = _platformOptions.Value.FileExtensionsWhiteList.Union(
+                _settingsManager?.GetObjectSettingAsync(PlatformConstants.Settings.Security.FileExtensionsWhiteList.Name).Result.AllowedValues.Cast<string>() ?? new string[0]);
+            var whitelisted = (whiteList.Any(x => path.Trim().EndsWith(x.Trim(), StringComparison.OrdinalIgnoreCase)));
+            var whiteListCount = whiteList.Count();
+
+            if (whiteListCount != 0)
+            {
+                if (blackListCount != 0 && blacklisted) return true;
+                return !whitelisted;
+            }
+            else
+            {
+                if (blackListCount != 0) return blacklisted;
+            }
+            return false;
         }
     }
 }
