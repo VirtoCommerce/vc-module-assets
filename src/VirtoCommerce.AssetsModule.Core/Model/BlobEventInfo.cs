76daedb085cb7343a126cd53c1a77e1e01a8fac1
artem-dudarev <ad@virtoway.com>
2024-02-20 15:42:19 +0200

    Refactoring

--- src/VirtoCommerce.AssetsModule.Core/Model/BlobEventInfo.cs
+++ src/VirtoCommerce.AssetsModule.Core/Model/BlobEventInfo.cs
@@ -1,10 +1,9 @@
 using VirtoCommerce.Platform.Core.Common;
 
-namespace VirtoCommerce.AssetsModule.Core.Model
+namespace VirtoCommerce.AssetsModule.Core.Model;
+
+public class BlobEventInfo : Entity
 {
-    public class BlobEventInfo : Entity
-    {
-        public string Provider { get; set; }
-        public string Uri { get; set; }
-    }
+    public string Uri { get; set; }
+    public string Provider { get; set; }
 }
--- src/VirtoCommerce.AssetsModule.Core/Services/BlobUploadStream.cs
+++ src/VirtoCommerce.AssetsModule.Core/Services/BlobUploadStream.cs
@@ -2,92 +2,90 @@ using System.IO;
 using System.Threading.Tasks;
 using VirtoCommerce.AssetsModule.Core.Events;
 using VirtoCommerce.AssetsModule.Core.Model;
-using VirtoCommerce.Platform.Core.Common;
 using VirtoCommerce.Platform.Core.Events;
 
-namespace VirtoCommerce.AssetsModule.Core.Services
+namespace VirtoCommerce.AssetsModule.Core.Services;
+
+public class BlobUploadStream : Stream
 {
-    public class BlobUploadStream : Stream
+    private readonly Stream _innerStream;
+    private readonly string _assetProvider;
+    private readonly string _blobUrl;
+    private readonly IEventPublisher _eventPublisher;
+
+    public BlobUploadStream(string assetProvider, string blobUrl, IEventPublisher eventPublisher, Stream innerStream)
     {
-        private readonly Stream _innerStream;
-        private readonly string _assetProvider;
-        private readonly string _blobUrl;
-        private readonly IEventPublisher _eventPublisher;
+        _assetProvider = assetProvider;
+        _blobUrl = blobUrl;
+        _eventPublisher = eventPublisher;
+        _innerStream = innerStream;
+    }
 
-        public BlobUploadStream(string assetProvider, string blobUrl, IEventPublisher eventPublisher, Stream innerStream)
+    public override bool CanRead => _innerStream.CanRead;
+
+    public override bool CanSeek => _innerStream.CanSeek;
+
+    public override bool CanWrite => _innerStream.CanWrite;
+
+    public override long Length => _innerStream.Length;
+
+    public override long Position
+    {
+        get => _innerStream.Position;
+        set => _innerStream.Position = value;
+    }
+
+    public override void Flush()
+    {
+        _innerStream.Flush();
+    }
+
+    public override int Read(byte[] buffer, int offset, int count)
+    {
+        return _innerStream.Read(buffer, offset, count);
+    }
+
+    public override long Seek(long offset, SeekOrigin origin)
+    {
+        return _innerStream.Seek(offset, origin);
+    }
+
+    public override void SetLength(long value)
+    {
+        _innerStream.SetLength(value);
+    }
+
+    public override void Write(byte[] buffer, int offset, int count)
+    {
+        _innerStream.Write(buffer, offset, count);
+    }
+
+    protected override void Dispose(bool disposing)
+    {
+        base.Dispose(disposing);
+
+        if (disposing)
         {
-            _assetProvider = assetProvider;
-            _blobUrl = blobUrl;
-            _eventPublisher = eventPublisher;
-            _innerStream = innerStream;
+            _innerStream.Dispose();
+
+            RaiseBlobCreatedEvent().GetAwaiter().GetResult();
         }
+    }
 
-        public override bool CanRead => _innerStream.CanRead;
-
-        public override bool CanSeek => _innerStream.CanSeek;
-
-        public override bool CanWrite => _innerStream.CanWrite;
-
-        public override long Length => _innerStream.Length;
-
-        public override long Position
+    protected virtual Task RaiseBlobCreatedEvent()
+    {
+        if (_eventPublisher is null)
         {
-            get => _innerStream.Position;
-            set => _innerStream.Position = value;
-        }
-
-        public override void Flush()
-        {
-            _innerStream.Flush();
-        }
-
-        public override int Read(byte[] buffer, int offset, int count)
-        {
-            return _innerStream.Read(buffer, offset, count);
-        }
-
-        public override long Seek(long offset, SeekOrigin origin)
-        {
-            return _innerStream.Seek(offset, origin);
-        }
-
-        public override void SetLength(long value)
-        {
-            _innerStream.SetLength(value);
-        }
-
-        public override void Write(byte[] buffer, int offset, int count)
-        {
-            _innerStream.Write(buffer, offset, count);
-        }
-
-        protected override void Dispose(bool disposing)
-        {
-            base.Dispose(disposing);
-
-            if (disposing)
-            {
-                _innerStream.Dispose();
-
-                RaiseBlobCreatedEvent().GetAwaiter().GetResult();
-            }
-        }
-
-        protected virtual Task RaiseBlobCreatedEvent()
-        {
-            if (_eventPublisher != null)
-            {
-                var eventData = new BlobEventInfo
-                {
-                    Id = _blobUrl,
-                    Uri = _blobUrl,
-                    Provider = _assetProvider,
-                };
-
-                return _eventPublisher.Publish(new BlobCreatedEvent([new GenericChangedEntry<BlobEventInfo>(eventData, EntryState.Added)]));
-            }
-
             return Task.CompletedTask;
         }
+
+        var eventData = new BlobEventInfo
+        {
+            Id = _blobUrl,
+            Uri = _blobUrl,
+            Provider = _assetProvider,
+        };
+
+        return _eventPublisher.Publish(new BlobCreatedEvent(eventData));
     }
 }
